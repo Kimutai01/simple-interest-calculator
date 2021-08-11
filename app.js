88d8590d09178d280dc74ef977b060a2c89489e2
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-08-11 16:21:09 -0400

    extracting the values

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ document.getElementById('form-group').addEventListener('submit', calculateResult
 
 function calculateResults(e){
     let amount =document.getElementById('amount');
-    let interest = document.getElementById('interest');
+    let interest =document.getElementById('interest');
     let time = document.getElementById('years');
     let monthlyPay = document.getElementById('monthly-payment');
     let totalPay = document.getElementById('total-payment');
     let totalInt =document.getElementById('total-interest')
+
+    let amou = parseInt(amount.value)
+    alert(amou)
+
+    let intr = parseInt(interest.value);
+    alert(intr)
+
+    let year = parseInt(years.value)
+    alert(year)
+
+    let inter=(intr/100) * amou;
+    alert(inter)
+
+
+   
 
     e.preventDefault()
 }
